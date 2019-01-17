bb75adf294d8f86e1532690d4db254feac5b1ec4
LukeyBeachBoy <lb580@kent.ac.uk>
2019-01-17 23:54:36 +0000

    Random updates

---
+++ dist/index.js
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const WebSocket = require("ws");
+const wss = new WebSocket.Server({ port: 3000, clientTracking: true });
+const clients = [];
+wss.on("connection", ws => {
+    clients.push(ws);
+    ws.on("message", (message) => {
+        console.log(`Received: ${message}`);
+        if (message == "unlock") {
+            console.log("Sending door unlock request");
+        }
+        else {
+            console.log("Sending door lock request");
+        }
+        clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+            else {
+                const index = clients.indexOf(ws);
+                clients.splice(index, 1);
+            }
+        });
+    });
+    console.log("Device Connected");
+    ws.send("Welcome");
+});
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,25 @@
-import * as WebSocket from 'ws';
+import * as WebSocket from "ws";
 
-const wss = new WebSocket.Server({port: 3000, clientTracking:true});
+const wss = new WebSocket.Server({ port: 3000, clientTracking: true });
 const clients = [];
-wss.on('connection', (ws) => {
-    clients.push(ws);
-    ws.on('message', (message:string) => {
-        console.log(`Received: ${message}`);
-        if (message == 'unlock') {
-            console.log('Sending door unlock request');
-        } else {
-            console.log('Sending door lock request');
-        }
-        clients.forEach((client) => {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(message);
-              }
-            else {
-                const index = clients.indexOf(ws);
-                clients.splice(index, 1);
-            }
-        })
+wss.on("connection", ws => {
+  clients.push(ws);
+  ws.on("message", (message: string) => {
+    console.log(`Received: ${message}`);
+    if (message == "unlock") {
+      console.log("Sending door unlock request");
+    } else {
+      console.log("Sending door lock request");
+    }
+    clients.forEach(client => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      } else {
+        const index = clients.indexOf(ws);
+        clients.splice(index, 1);
+      }
     });
-    console.log('Device Connected');
-    ws.send('Welcome');
+  });
+  console.log("Device Connected");
+  ws.send("Welcome");
 });
